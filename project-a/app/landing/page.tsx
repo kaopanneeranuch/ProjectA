91ab6f6f0680b89ab906e47bd8c93324aeebf66e
Teerawat chuaphanngam <99793968+phukaokkSIIT@users.noreply.github.com>
2023-11-05 16:13:39 +0700

    Initial create next.js app
    Initially created next.js App base on typescript and tailwind.css

---
+++ project-a/app/landing/page.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Project A</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <span className="block">Welcome to</span>
+              <span className="block text-indigo-600">Project A</span>
+            </h1>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
+              A sample landing page built with Tailwind CSS on Next.js
+            </p>
+            <div className="mt-5 sm:mt-8 sm:flex justify-center">
+              <div className="rounded-md shadow">
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                >
+                  Get started
+                </a>
+              </div>
+              <div className="mt-3 sm:mt-0 sm:ml-3">
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
